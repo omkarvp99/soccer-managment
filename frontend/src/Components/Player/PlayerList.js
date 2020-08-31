8b867da1573e3390ebe0c35549a92f5eb08962c1
omkar <omkvp@outlook.com>
2020-08-31 13:55:21 +0530

    added frontend folder succesfully

---
+++ frontend/src/Components/Player/PlayerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+const PlayerList = (props) => {
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4>Players</h4>
+        </li>
+        {props.players.map((item) => (
+          <a
+            href="#!"
+            className="collection-item"
+            key={item._id}
+            onClick={props.updateCurrentPlayer.bind(this, item)}
+          >
+            {item.firstName} {item.lastName}
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerList;
---
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const PlayerSingle = (props) => {
+  return (
+    <div className="row">
+      <div className="col s12">
+        <div className="card">
+          <div className="card-image">
+            <img src="soccer.jpg" alt="soccer img" />
+            <span className="card-title">
+              {props.player.firstName}
+              {props.player.lastName}
+            </span>
+          </div>
+          <div className="card-content">
+            <p>
+              Phone: {props.player.phone} - Email: {props.player.email}
+            </p>
+            <p>
+              Strength: {props.player.strength} - Endurance:{" "}
+              {props.player.endurance}
+            </p>
+          </div>
+          <div className="card-action">Team: {props.player.team}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerSingle;
